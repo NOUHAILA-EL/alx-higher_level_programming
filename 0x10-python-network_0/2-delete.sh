e7ee91631d5368d62cf0b192275be0fe457e55ad
NOUHAILA EL ATOUBI <56658946+NOUHAILA-EL@users.noreply.github.com>
2023-11-30 18:32:50 +0100

    Add files via upload

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script to send delete request to the url passed and displays reponse
-curl -s -X DELETE "$1"
+# sends a DELETE request to the URL passed as the first argument and displays the body of the response
+curl -sX DELETE $1 -L
