c4e57231b24a2940da759f019ff49a225ca86823
NOUHAILA EL ATOUBI <56658946+NOUHAILA-EL@users.noreply.github.com>
2023-12-01 23:28:50 +0100

    Add files via upload

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
 """
-takes in a URL, sends a request to the URL and displays the
-body of the response using requests
+Python Script that takes in a URL, sends a request to the URL
+& displays the body of the response
 """
 import requests
 from sys import argv
 
-
-if __name__ == "__main__":
-    """
-    takes in a URL, sends a request to the URL and displays the
-    body of the response using requests
-    """
-    url = argv[1]
-    r = requests.get(url)
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
-    else:
-        print(r.text)
+if __name__ == '__main__':
+    r = requests.get(argv[1])
+    status = r.status_code
+    print(r.text) if status < 400 else print(
+        "Error code: {}".format(r.status_code))
